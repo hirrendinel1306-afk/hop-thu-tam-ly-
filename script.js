c0850b94f10366c5f56b0fbfdda55288058a80c5
hirrendinel1306-afk <hirren.dinel1306@gmail.com>
2026-07-29 20:12:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+// Website Hộp thư Tâm lý
+
+console.log("Website Hộp thư Tâm lý đã khởi động thành công!");
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", function () {
+
+    alert("Chức năng trò chuyện sẽ được phát triển ở bài học tiếp theo.");
+
+});
